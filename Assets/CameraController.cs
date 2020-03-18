b12c926ed6a968de63a775e7b16724fc35d6d52b
tspt8 <tspt89@hotmail.com>
2020-03-17 23:19:42 -0600

    Second Commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform target;
+    public float speed = 2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        //Follow the player in horizontal axis
+        //transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+
+        //Moving up alone
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+}
